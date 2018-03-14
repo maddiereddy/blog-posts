7746d22c95b7b4dec658246319077ab8e85f4ab4
maddiereddy <maddiereddy@gmail.com>
2018-03-14 00:00:23 -0700

    created POST endpoint

--- server.js
+++ server.js
@@ -24,10 +24,28 @@ BlogPosts.create('For Sale', 'Selling an iPad mini for $100', 'Maddie R');
 BlogPosts.create('Recipe', 'Need a recipe for tomato soup', 'Sammy Hernandez');
 BlogPosts.create('Free', 'Free rose bush for anyone interested', 'Grace Buchanan');
 
+
+
 // when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
+// all current BlogPosts items by calling `BlogPosts.get()`
 app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
+});
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
 });
 
 app.listen(process.env.PORT || 8080, () => {
